0f3fe75d232112c2bc46c99d09dbb7a67c98875c
Anidhya Ahuja <anidhya@weboniselab.com>
2011-08-10 18:23:10 +0530

    good hr

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodhr::Application.config.secret_token = '840b8b9cdc25685c0558445d003e704156765a46d29286f5f1f43a149a869b7bb31506aa11542f02b1f5dd51f1df9174a2be0fa3add7978ad7b1d990adf77970'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Goodhr::Application.config.session_store :cookie_store, :key => '_goodhr_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Goodhr::Application.config.session_store :active_record_store
